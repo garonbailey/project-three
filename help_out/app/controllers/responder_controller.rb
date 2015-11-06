fdd7616510197b1e222074aea3ff0aab9b4b45bb
Sean <sean@seans-macbook.home>
2015-11-05 22:22:16 -0500

    built out create method for responder for sake of bcrypt

--- help_out/app/controllers/responder_controller.rb
+++ help_out/app/controllers/responder_controller.rb
@@ -1,7 +1,21 @@
 class ResponderController < ApplicationController
   def new
+    @responder = Responder.new
   end
 
   def create
+    @responder = Responder.new(responder_params)
+    if @responder.save
+      session[:responder_id] = @responder.id
+      redirect_to new_session_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def responder_params
+    return params.require(:responder).permit(:username, :password)
   end
 end
