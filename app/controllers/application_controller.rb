b0fcff7facc18c4a85ca4aa2f8742e5a9092ae1a
Tucker Margulies <marguliw@gmail.com>
2015-11-06 15:11:18 -0500

    api key formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,6 @@ class ApplicationController < ActionController::Base
 
   def index
     puts "https://maps.googleapis.com/maps/api/js?key=" + ENV['GOOGLE_MAP'] + "&callback=initMap"
-
-
-    puts "some garbages"
-
     @map_url = "https://maps.googleapis.com/maps/api/js?key=" + ENV['GOOGLE_MAP'] + "&callback=initMap"
 
     render '/index'
