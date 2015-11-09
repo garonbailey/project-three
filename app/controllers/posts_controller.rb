753f31f72193a761509d5201171dd37cd97aa593
Tucker Margulies <marguliw@gmail.com>
2015-11-09 09:34:34 -0500

    work from the weekend

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,11 @@
 class PostsController < ApplicationController
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+  end
+
+  def post_params
+    return params.require(:post).permit(:description, :latitude, :longitude)
+
 end
