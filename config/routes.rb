5ed07e80ed4e7e67bdf8676ddb0870ca8d3de282
garonbailey <bailegm@gmail.com>
2015-11-09 09:57:50 -0500

    updated route and controller names

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
 
   root 'application#angular'
 
@@ -9,16 +8,20 @@ Rails.application.routes.draw do
 
   get '/' => 'posts#index'
 
+  get '/login' => ''
+
+  get '/reports' => ''
+
+  get '/closed' => ''
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-=======
-  root 'application#index'
 
   get '/portal' => 'application#portal'
-  get '/sessions' => 'sessions#current_responder', defaults: { format: :json }
-  post '/sessions' => 'sessions#create'
-  delete '/sessions' => 'sessions#destroy'
->>>>>>> a757f25f2032fc79e5817a1bee9a60f6a94fdd91
+  get '/session' => 'session#current_responder', defaults: { format: :json }
+  post '/session' => 'session#create'
+  delete '/session' => 'session#destroy'
+
 
 
 
