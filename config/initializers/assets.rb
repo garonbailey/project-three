fde8540226acc541fbf40e73989cf0f3aacf9a4a
garonbailey <bailegm@gmail.com>
2015-11-12 19:41:06 -0500

    heroku troubleshooting again

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,8 +2,8 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( styles.css )
-Rails.application.config.assets.precompile += %w( login.css )
+# Rails.application.config.assets.precompile += %w( styles.css )
+# Rails.application.config.assets.precompile += %w( login.css )
 Rails.application.config.assets.precompile += %w( scroll.js )
 
 # Add additional assets to the asset load path
